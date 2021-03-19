0dc44d1388d775f71bf1430456f9f14133645710
safarov-e <elkhan.safarov10@yandex.com>
2021-03-19 13:56:38 +0500

    Слайдер

--- src/components/channel-list/channel-list.js
+++ src/components/channel-list/channel-list.js
@@ -4,23 +4,49 @@ import { channelsFetchData } from '../../store/actions/channels';
 import './channel-list.css';
 
 class ChannelList extends Component {
+    state = {
+        left: 0,
+        right: 0,
+    }
     componentDidMount() {
         this.props.fetchData("http://epg.domru.ru/channeltheme/list")
     }
+    sliderLeft = () => {
+        if(this.state.left >= -900){
+            this.setState(({left}) => {
+                return {left: left - 102}
+            })
+        }
+    }
+    sliderRight = () => {
+        if(this.state.left < 0) {
+            this.setState(({left}) => {
+                return {left: left + 102}
+            })
+        }
+    }
     render() {
         const {channels} = this.props;
+        const {left} = this.state;
+        console.log(left)
         return (
             <div className="channel-list">
                 <div className="channel-list-group">
                     <div className="channel-list-slider">
                         <h2>Темы каналов</h2>
                         <div className="action-button-slider">
-                            <button className="slider-arrow-left" title="Предыдущий слайд"></button>
-                            <button className="slider-arrow-right" title="Следующий слайд"></button>
+                            <button 
+                                onClick={this.sliderLeft} 
+                                className="slider-arrow-left" 
+                                title="Предыдущий слайд"></button>
+                            <button 
+                                onClick={this.sliderRight} 
+                                className="slider-arrow-right"
+                                title="Следующий слайд"></button>
                         </div>
                     </div>
-                    <div className="channel-list-group">
-                        <ul>
+                    <div className="channel-list__slider">
+                        <ul style={{left: left + 'px'}}>
                             {
                                 channels.map((item, index) => {
                                     return <li key={index}>{item.name}</li>
